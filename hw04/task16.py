68ffa7e49472848ece8efb6a3a0d0380f0e906ac
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-05-01 16:13:13 +0600

    Update task16.py

--- hw04/task16.py
+++ hw04/task16.py
@@ -3,14 +3,14 @@ sum = n
 avg = 0
 max = n
 min = n
-for i in range(1, 20):
+for i in range(1, 10):
     m = float(input('Enter number:\n'))
     if (m>max):
         max = m
     elif (m<min):
         min = m
     sum+=m
-avg = sum / 20.0
+avg = sum / 10.0
 print('Sum is ' + str(sum))
 print('Average is '+ str(avg))
 print('Maximum number is '+str(max))
