817a54f30fb9fc0f1381893bb12fc171109f6941
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-05-01 13:23:23 +0600

    changes

--- hw04/task11.py
+++ hw04/task11.py
@@ -1,10 +1,10 @@
 sum = 0
-oddCount = 0
+evenCount = 0
 for i in range(1, 11):
     x = int(input('Enter number:\n'))
-    if (x%2!=0):
+    if (x%2==0):
         sum+=x
-        oddCount += 1
+        evenCount += 1
 print('Sum is ' +str(sum))
-avg = sum/oddCount
+avg = sum/evenCount
 print('Average is '+str(avg))
