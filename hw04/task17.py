d7c14082de07a4eb254384c7481bc21a10f986a9
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-05-01 17:43:26 +0600

    changes

---
+++ hw04/task17.py
@@ -0,0 +1,19 @@
+nterms = 10
+
+a = 0
+b = 1
+count = 0
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(a)
+else:
+   print("Fibonacci sequence upto",nterms,":")
+   while (count < nterms):
+       print(a, end=', ')
+       nth = a + b
+       a = b
+       b = nth
+       count += 1
