e94fb96fd19de2527002747c68478da34ad1c6d4
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-10 22:32:40 +0600

    Update task11.py

--- hw03/task11.py
+++ hw03/task11.py
@@ -2,6 +2,6 @@ mark = float(input('Enter the mark of quiz:\n'))
 currentScale = float(input('Enter the current scale:\n'))
 targetScale = float(input('Enter the target scale:\n'))
 
-z = (mark*targetScale)/currentScale
+z = (mark * targetScale) / currentScale
 
-print('The target scale mark is '+str(z))
+print('The target scale mark is ' + str(z))
