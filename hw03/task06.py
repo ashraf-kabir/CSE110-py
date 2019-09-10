3c7fa9fe2cc95deecb911e1143f56e489fb6fb64
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-10 22:30:08 +0600

    Update task06.py

--- hw03/task06.py
+++ hw03/task06.py
@@ -1,8 +1,8 @@
 s = float(input('Enter a number:\n'))
 
-if s<100:
-    l = 3000 - (125 * (s*s))
+if s < 100:
+    l = 3000 - (125 * (s * s))
     print(l)
-elif s>=100:
-    l = 12000/ (4 + ((s*s) / 14900))
+elif s >= 100:
+    l = 12000 / (4 + ((s * s) / 14900))
     print(l)
