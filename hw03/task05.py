7335349b69fa9f664f6bbb0e3cde50e6eb3b44ae
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-10 22:27:48 +0600

    Update task05.py

--- hw03/task05.py
+++ hw03/task05.py
@@ -4,4 +4,4 @@ remSec = seconds % 60
 hours = mins / 60
 remMins = mins % 60
 
-print(str(int(hours))+' hours, '+str(int(remMins))+' mins, '+str(remSec)+' seconds')
+print(str(int(hours)) + ' hours, ' + str(int(remMins)) + ' mins, ' + str(remSec) + ' seconds')
