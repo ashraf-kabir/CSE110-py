88116ead9805f3ecd6dae6cf10ec56189323056a
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-10 22:30:28 +0600

    Update task07.py

--- hw03/task07.py
+++ hw03/task07.py
@@ -4,8 +4,8 @@ x = float(input('Enter the value of first side of triangle\n'))
 y = float(input('Enter the value of second side of triangle\n'))
 z = float(input('Enter the value of third side of triangle\n'))
 
-s = ((x+y+z)/2)
+s = ((x + y + z) / 2)
 
-area = math.sqrt(s*(s-x)*(s-y)*(s-z))
+area = math.sqrt(s * (s - x) * (s - y) * (s - z))
 
-print('Area of the triangle is '+str(area))
+print('Area of the triangle is ' + str(area))
