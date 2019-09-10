b8d25923e51e98d1fe67f6cafd31e7a7c1cd4f7f
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-10 22:27:23 +0600

    Update task04.py

--- hw03/task04.py
+++ hw03/task04.py
@@ -2,15 +2,15 @@ payment = float(input('Enter your payment:\n'))
 age = int(input('Enter your age:\n'))
 tax = 0
 
-if (age < 18):
+if age < 18:
     print('no tax')
 
-elif (age >=18):
-    if (payment <10000):
+elif age >= 18:
+    if payment < 10000:
         print('no tax')
     elif (payment >= 10000) and (payment <= 20000):
-        tax = payment * 5/100
+        tax = payment * 5 / 100
         print(tax)
-    elif (payment > 20000):
-        tax = payment * 10/100
+    elif payment > 20000:
+        tax = payment * 10 / 100
         print(tax)
