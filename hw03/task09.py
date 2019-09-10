ef3b28f017885d0dfd747b90520e21739d71ee6e
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-10 22:31:14 +0600

    Update task09.py

--- hw03/task09.py
+++ hw03/task09.py
@@ -3,8 +3,8 @@ num2 = float(input('Enter the second number:\n'))
 num3 = float(input('Enter the third number:\n'))
 
 if (num1 > num2) and (num1 > num3):
-    print('First number ('+ str(num1) + ') is the largest')
+    print('First number (' + str(num1) + ') is the largest')
 elif (num2 > num1) and (num2 > num3):
-    print('Second number ('+ str(num2) + ') is the largest')
+    print('Second number (' + str(num2) + ') is the largest')
 else:
-    print('Third number ('+ str(num3) + ') is the largest')
+    print('Third number (' + str(num3) + ') is the largest')
